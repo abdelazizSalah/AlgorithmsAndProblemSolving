8fe463ec13e70185456d8a79fcea17f50b53d7cf
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-02-13 14:16:04 +0200

    solving chocolate problem

---
+++ ProblemsOfCategoryB/B.Chocolate.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long n, res = 1;
+    bool flag = false;
+    cin >> n;
+    int arr[105];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] == 1)
+            flag = true;
+    }
+
+    if (!flag)
+        return cout << 0, 0;
+
+    for (int i = 0; i < n; i++)
+        if (arr[i] == 1)
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] == 1)
+                {
+                    res = res * (j - i);
+                    break;
+                }
+
+    cout << res;
+    return 0;
+}
