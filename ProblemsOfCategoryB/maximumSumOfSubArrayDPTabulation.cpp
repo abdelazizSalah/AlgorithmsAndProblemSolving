bc7ea49d3722075dc7ffe471baf3b17db4a06529
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-03-03 12:19:56 +0200

    solving dp problem using tabulation.

---
+++ ProblemsOfCategoryB/maximumSumOfSubArrayDPTabulation.cpp
@@ -0,0 +1,27 @@
+/// DP SOLVERRRRRR (TABULATION)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int> &nums)
+{
+    int sz = nums.size();
+    vector<int> res(sz);
+    res[sz - 1] = nums[sz - 1];
+    for (int i = sz - 2; i > -1; i--)
+    {
+        res[i] = max(res[i + 1] + nums[i], nums[i]);
+    }
+    return *max_element(res.begin(), res.end());
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int &n : nums)
+        cin >> n;
+
+    cout << maxSubArray(nums);
+}
