702130a6ed11550121416756703bf0841e7fa7db
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-07-13 16:09:14 +0300

    solving the problem using tabulation

--- DynamicProgramming/DeleteAndEarn.cpp
+++ DynamicProgramming/DeleteAndEarn.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 #define DPSolver ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
+// el mushkela delw2ty fl backtracking :(
+
 map<pair<int, pair<int, int>>, int> memo;
 int solve(const vector<int> &vals, vector<bool> &vis, int val, int idx, int lvl, int parent)
 {
@@ -52,23 +54,38 @@ int solve(const vector<int> &vals, vector<bool> &vis, int val, int idx, int lvl,
     return sum;
 }
 
+int tabulationAlgo(vector<int> &vals);
 int main()
 {
     DPSolver;
-    vector<bool> vis(1e9);
+    // vector<bool> vis(1e9);
     int n;
     cin >> n;
     vector<int> vals(n);
     for (int &val : vals)
         cin >> val;
+    cout << tabulationAlgo(vals) << endl;
 
-    int mx = INT_MIN;
-    for (int i = 0; i < n; i++)
-    {
-        swap(vals[0], vals[i]);
-        mx = max(mx, solve(vals, vis, vals[0], 0, 1, -1));
-    }
+    // int mx = INT_MIN;
 
-    cout << mx << endl;
-    return 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     swap(vals[0], vals[i]);
+    //     mx = max(mx, solve(vals, vis, vals[0], 0, 1, -1));
+    // }
+
+    // cout << mx << endl;
+    // return 0;
+}
+
+int tabulationAlgo(vector<int> &vals)
+{
+    int n = *max_element(vals.begin(), vals.end());
+    vector<int> nums(n + 1);
+    for (int num : vals)
+        nums[num] += num;
+    for (int i = 2; i <= n; i++)
+        nums[i] = max(nums[i] + nums[i - 2], nums[i - 1]);
+
+    return nums[n];
 }
