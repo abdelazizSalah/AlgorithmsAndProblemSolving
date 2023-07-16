d8c5caa734ee783b18a11517b70210ec98f6688c
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-07-16 12:18:02 +0300

    solving a shortest paths problem using bfs

--- Graphs/ShortestPathes/Problems/shortestPathWithAlternatingColors.cpp
+++ Graphs/ShortestPathes/Problems/shortestPathWithAlternatingColors.cpp
@@ -1,3 +1,99 @@
 // logic of the problem:
 // find shortest path using dijekstra
 // check if this path has alternating colors.
+/*
+    @Algorithm
+        1. Read the Inputs
+        2. build the graph in list representation, with keeping the color instead of the weight
+        3. create a visited vector
+        4. create a distance vector and initialize it with -1
+        5. set D[0] = 0, because we will always start from node 0
+        6. apply BFS
+            1. to insert a node in the queue, the color of the edge should be different than the color of the current node
+            2. initialy insert the color of node 0 as black.
+            3. after visiting all the node's neighbours, increment the level.
+            4. mark the node as visited, and store the distance [node] = lvl
+        7. return the distance vector.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define DPSolver ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+bool canInsert(pair<int, char> neighbour, char nodeColor)
+{
+    return nodeColor != neighbour.second;
+}
+
+void bfs(const vector<vector<pair<int, char>>> &G, vector<int> &distances, vector<bool> &vis)
+{
+    queue<pair<int, char>> queue;
+    queue.push({0, 'b'});
+    int lvl = 1;
+    while (queue.size())
+    {
+        // extract the node from the queue
+        auto node = queue.front();
+        queue.pop();
+        for (auto nei : G[node.first])
+            // insert the neibour into the queue under our condition
+            if (canInsert(nei, node.second))
+            {
+                // insert into the queue
+                queue.push(nei);
+
+                // set the distance of the neighbour
+                distances[nei.first] = lvl;
+            }
+        // mark the node as finshed
+        vis[node.first] = true;
+
+        // increment the level.
+        lvl++;
+    }
+}
+void printGraph(const vector<vector<pair<int, char>>> &G)
+{
+    for (int i = 0; i < G.size(); i++)
+    {
+        cout << "Neighbours of node " << i << " are: \n";
+        for (auto nei : G[i])
+            cout << "nei #: " << nei.first << " with edge color: " << nei.second;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    DPSolver;
+    int noOfNodes, noOfRedEdges, noOfBlueEdges;
+    cin >> noOfNodes >> noOfRedEdges >> noOfBlueEdges;
+    vector<vector<pair<int, char>>> G(noOfNodes);
+    // insert red Nodes first
+    for (int i = 0; i < noOfRedEdges; i++)
+    {
+        int src, dest;
+        cin >> src >> dest;
+        G[src].push_back({dest, 'R'});
+    }
+
+    // insert blue Nodes first
+    for (int i = 0; i < noOfBlueEdges; i++)
+    {
+        int src, dest;
+        cin >> src >> dest;
+        G[src].push_back({dest, 'B'});
+    }
+
+    // print the Graph
+    printGraph(G);
+
+    // apply bfs;
+    vector<int> distances(noOfNodes, -1);
+    distances[0] = 0;
+    vector<bool> visited(noOfNodes);
+    bfs(G, distances, visited);
+    for (auto dis : distances)
+        cout << dis << ' ';
+    return 0;
+}
