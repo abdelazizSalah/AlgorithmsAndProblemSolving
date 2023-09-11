ad4450c046f87ab3653ed6faef2715d2223aca5e
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-09-11 18:32:04 +0300

    solving a binary search problem

--- LeetCodeProblems/RandomProblems/153.FindMinimumInRotatedSortedArray.cpp
+++ LeetCodeProblems/RandomProblems/153.FindMinimumInRotatedSortedArray.cpp
@@ -7,32 +7,31 @@ using namespace std;
 
 int findMin(vector<int> &nums)
 {
-
     int sz = nums.size();
-    if (sz == 1)
-        return nums[0];
     int bgn = 0;
-    int end = sz;
-
-    int md = (end + bgn) / 2;
+    int end = sz - 1;
     int mn = nums[0];
-    while (end > bgn && md != 0 && md != sz - 1)
+    int md = (end + bgn) / 2;
+    while (end > bgn)
     {
-        // apply binary search
-        if (nums[md] > nums[md - 1] && nums[md] > nums[md + 1])
-            // the number is on the md right
-            bgn = md + 1;
-        // else if (nums[md] > nums[md - 1] && nums[md] < nums[md + 1])
-        //     end = md;
-        else if (nums[md] > nums[md - 1] && nums[md] < nums[md + 1]) // normal place
-            if (nums[md] > nums[bgn])
-                end = md;
-            else
-                bgn = md + 1;
-        md = (end + bgn) / 2;
-        mn = nums[md];
-    }
+        if (nums[md] < nums[bgn] && nums[md] < nums[end])
+            end = md;
 
+        else if (nums[md] > nums[bgn] && nums[md] > nums[end])
+            bgn = md + 1;
+        else if (nums[md] > nums[bgn] && nums[md] < nums[end])
+            end = md;
+        else
+        {
+            mn = min(nums[md + 1], nums[md]);
+            break;
+        }
+        if (nums[md] < mn)
+            mn = nums[md];
+        md = (end + bgn) / 2;
+    }
+    if (md < sz && md > 0)
+        mn = min(mn, nums[md]);
     return mn;
 }
 
@@ -40,8 +39,8 @@ int main()
 {
     DPSolver;
     // vector<int> nums = {3, 4, 5, 1, 2};
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-    // vector<int> nums = {11, 13, 15, 17};
+    // vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums = {11, 13, 15, 17};
     cout << findMin(nums);
     return 0;
 }
