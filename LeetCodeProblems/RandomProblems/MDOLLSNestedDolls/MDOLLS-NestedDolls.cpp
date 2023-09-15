7531836bad83c51480c002b46306c3b15d64cb7b
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-09-15 18:19:17 +0300

    now algorithm is working lets make it efficent

---
+++ LeetCodeProblems/RandomProblems/MDOLLSNestedDolls/MDOLLS-NestedDolls.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define DPSolver ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+bool getBigger(pair<pair<int, int>, vector<int>> &p1, pair<pair<int, int>, vector<int>> &p2)
+{
+    return p1.second.size() >= p2.second.size();
+}
+
+pair<pair<int, int>, vector<int>> countMaxDolls(vector<pair<int, int>> &dolls, const int m, vector<bool> &taken, int idx)
+{
+    // define base case
+    if (idx >= m)
+        return {{INT_MAX, INT_MAX}, {}};
+    else if (idx == m - 1)
+        return {dolls[idx], {idx}};
+
+    // normal case
+    priority_queue<pair<pair<int, int>, vector<int>>> PossibleResults;
+    for (int i = idx + 1; i < m; i++)
+    {
+        if (!taken[i])
+        {
+            pair<pair<int, int>, vector<int>> R = countMaxDolls(dolls, m, taken, i);
+            if (PossibleResults.empty())
+                PossibleResults.push(R);
+            else
+            {
+                auto top = PossibleResults.top();
+                if (getBigger(R, top))
+                    // R is bigger
+                    PossibleResults.push(R);
+            }
+        }
+    }
+    if (PossibleResults.size())
+    {
+        int mxSize = PossibleResults.top().second.size();
+        pair<pair<int, int>, vector<int>> top = PossibleResults.top();
+        while (PossibleResults.size())
+        {
+            auto top = PossibleResults.top();
+            if (top.second.size() < mxSize)
+                break;
+            else
+            {
+                if (dolls[idx].first > top.first.first && dolls[idx].second > top.first.second)
+                {
+                    top.second.push_back(idx);
+                    return {dolls[idx], top.second};
+                }
+            }
+            PossibleResults.pop();
+        }
+        return top;
+    }
+    else
+
+        return {dolls[idx], {idx}};
+}
+
+void solve()
+{
+    int m;
+    cin >> m;
+    vector<pair<int, int>> dolls(m);
+    for (int i = 0; i < m; i++)
+    {
+        int w, h;
+        cin >> w >> h;
+        dolls[i] = {w, h};
+    }
+    sort(dolls.begin(), dolls.end(), greater<pair<int, int>>());
+    vector<bool> taken(m);
+    int numberOfDolls = 0;
+    int numberOfIterations = 0;
+    int i = 0;
+    while (i < m && numberOfIterations < m)
+        if (!taken[i])
+        {
+            numberOfDolls++;
+            auto R = countMaxDolls(dolls, m, taken, i);
+            for (auto idx : R.second)
+                taken[idx] = true;
+            if (taken[i] == 0)
+                auto R = countMaxDolls(dolls, m, taken, i);
+            else
+                i++;
+
+            numberOfIterations++;
+        }
+        else
+        {
+            i++;
+            numberOfIterations++;
+        }
+
+    cout << numberOfDolls << '\n';
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
