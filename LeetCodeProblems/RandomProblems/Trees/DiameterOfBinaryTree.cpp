b9199348588eac56b6aae852f9e34c0f961f7eaa
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-09-20 00:34:16 +0300

    Finding the Diameter of a tree.

--- LeetCodeProblems/RandomProblems/Trees/DiameterOfBinaryTree.cpp
+++ LeetCodeProblems/RandomProblems/Trees/DiameterOfBinaryTree.cpp
@@ -13,3 +13,48 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+int diameterOfBinaryTreeWithBool(TreeNode *root, bool &strt, int &mx, int cntr)
+{
+    if (root == nullptr)
+        return -1;
+    else if (root->left == nullptr && root->right == nullptr)
+    {
+        mx = max(mx, 0);
+        return 0;
+    }
+
+    int left = diameterOfBinaryTreeWithBool(root->left, strt, mx, cntr + strt);
+    int right = diameterOfBinaryTreeWithBool(root->right, strt, mx, left + strt);
+    int h = 1 + max(left, right);
+    int d = left + right + 1 + 1;
+    mx = max(d, mx);
+    return h;
+}
+int diameterOfBinaryTree(TreeNode *root)
+{
+    DPSolver;
+    bool strt = false;
+    int mx = INT_MIN;
+    int cntr = 0;
+    diameterOfBinaryTreeWithBool(root, strt, mx, cntr);
+    return mx;
+}
+
+int main()
+{
+    // create root = 1
+    // root -> left = 2, root -> right = 3
+    // root -> left -> left = 4, root -> left -> right = 5
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    // root->right->left = new TreeNode(6);
+
+    cout << diameterOfBinaryTree(root) << endl;
+    return 0;
+}
+// solution video link.
+// https://www.youtube.com/watch?v=bkxqA8Rfv04
