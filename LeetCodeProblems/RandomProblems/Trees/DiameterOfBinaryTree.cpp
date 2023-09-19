324d5ef459bfd136504095b6022c1edb7e4d595b
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-09-19 22:57:46 +0300

    trying to solve a problem on trees

---
+++ LeetCodeProblems/RandomProblems/Trees/DiameterOfBinaryTree.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define DPSolver                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
