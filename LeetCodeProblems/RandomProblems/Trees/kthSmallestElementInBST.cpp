cb1c6946b3091879bdcb84d40d18e4e8445db329
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-09-25 21:20:34 +0300

    trying another solution

--- LeetCodeProblems/RandomProblems/Trees/kthSmallestElementInBST.cpp
+++ LeetCodeProblems/RandomProblems/Trees/kthSmallestElementInBST.cpp
@@ -13,51 +13,16 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-
-void findKth(TreeNode *root, int k, bool &found, bool &strt, int &res, int &cntr)
-{
-    // empty tree
-    if (!root)
-        return;
-    // leaf node
-    if (!root->left)
-    {
-        strt = true;
-        if (cntr == k)
-        {
-            res = root->val;
-            found = true;
-        }
-        return;
-    }
-    // we have not reached the left most node yet
-    if (!strt)
-        findKth(root->left, k, found, strt, res, cntr);
-    // if we are now standing on it.
-    if (cntr == k + 1)
-    {
-        res = root->val;
-        found = true;
-        return;
-    }
-    else
-    {
-        // base case
-        cntr++;
-        findKth(root->left, k, found, strt, res, cntr);
-        // if we have found it
-        if (found)
-            return;
-        findKth(root->right, k, found, strt, res, cntr);
-    }
-}
-
 int kthSmallest(TreeNode *root, int k)
 {
-    bool found = false;
-    bool strt = false;
-    int res = INT_MIN;
-    int cntr = 1;
-    findKth(root, k, found, strt, res, cntr);
-    return res;
+    if (!root)
+        return 1;
+    int left = kthSmallest(root->left, k);
+    int right = kthSmallest(root->right, k);
+    if (left == k)
+        return root->val;
+    else if (right == k)
+        return root->val;
+    else
+        return min(left, right) + 1;
 }
