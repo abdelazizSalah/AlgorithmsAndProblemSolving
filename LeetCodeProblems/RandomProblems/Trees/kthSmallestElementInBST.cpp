97b6f5480b862217cce3dc4353b44db47af7aae0
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-09-26 14:19:15 +0300

    finding the kth smalles element in BST

--- LeetCodeProblems/RandomProblems/Trees/kthSmallestElementInBST.cpp
+++ LeetCodeProblems/RandomProblems/Trees/kthSmallestElementInBST.cpp
@@ -13,16 +13,62 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+void dfs(TreeNode *root, queue<TreeNode *> &nodes)
+{
+    // empty tree
+    if (!root)
+        return;
+    // leaf node
+    if (!root->left && !root->right)
+    {
+        nodes.push(root);
+        return;
+    }
+    // has right child only
+    if (!root->left)
+    {
+        nodes.push(root);
+        dfs(root->right, nodes);
+    }
+    else if (!root->right)
+    {
+        // has left child only
+        dfs(root->left, nodes);
+        nodes.push(root);
+    }
+    else
+    {
+        // has both children
+        dfs(root->left, nodes);
+        nodes.push(root);
+        dfs(root->right, nodes);
+    }
+}
+
 int kthSmallest(TreeNode *root, int k)
 {
-    if (!root)
-        return 1;
-    int left = kthSmallest(root->left, k);
-    int right = kthSmallest(root->right, k);
-    if (left == k)
-        return root->val;
-    else if (right == k)
-        return root->val;
-    else
-        return min(left, right) + 1;
+    queue<TreeNode *> nodes;
+    dfs(root, nodes);
+    while (k--)
+        nodes.pop();
+    return nodes.front()->val;
+}
+
+// better solution  -> more compact, but same complexity.
+void inOrderTraversal(TreeNode *root, vector<int> &v)
+{
+    if (root == NULL)
+        return;
+
+    // left, root, right
+    inOrderTraversal(root->left, v);
+    v.push_back(root->val);
+    inOrderTraversal(root->right, v);
+}
+int kthSmallest(TreeNode *root, int k)
+{
+    vector<int> v;
+    inOrderTraversal(root, v);
+    return v[k - 1];
 }
