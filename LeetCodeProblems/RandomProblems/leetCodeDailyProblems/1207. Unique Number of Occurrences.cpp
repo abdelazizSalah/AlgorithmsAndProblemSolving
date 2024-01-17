81d8e695f894f440f5e54255806bdb0544df3eca
Abdelaziz Salah <abdelaziz132001@gmail.com>
2024-01-17 19:49:11 +0200

    accepted first time.

---
+++ LeetCodeProblems/RandomProblems/leetCodeDailyProblems/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast", "inline", "ffast-math", "unroll-loops", "no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native", "f16c")
+static const auto DPSolver = []()
+{ std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr); return 'c'; }();
+using namespace std;
+
+bool uniqueOccurrences(vector<int> &arr)
+{   
+    DPSolver; 
+    priority_queue<int> pq; 
+    map<int, int> mp; 
+    for (int a : arr) 
+        mp[a]++; 
+    
+    for (auto item : mp)
+        pq.push(item.second); 
+    
+    while(pq.size()){
+        int tp = pq.top(); 
+        pq.pop(); 
+        if (pq.size() && tp == pq.top())
+            return false; 
+    }
+    return true; 
+}
